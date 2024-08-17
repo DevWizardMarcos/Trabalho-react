204ce74fa192844539227ac2da306786620f2235
Marcos Paulo Simões <75817190+DevWizardMarcos@users.noreply.github.com>
2024-08-17 19:33:26 -0300

    Add files via upload

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import './css/card.css'
 
-function Card({title, image}) {
-	return(
-			<div className='card'>
-                <h4>{title}</h4>
-                <img className="cardImage" src={image} />
-                </div>
-    )
+import { useState } from 'react';
+
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+        <button onClick={onClose}>Fechar</button>
+      </div>
+    </div>
+  );
+}
+
+function Card({ title, image }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="card">
+      <h4 style={{ transform: 'translateZ(translate(-50%, -50%))' }}>{title}</h4>
+      <img className="cardImage" src={image} />
+      <button className="cardButton" onClick={handleButtonClick}></button>
+      <Modal isOpen={isOpen} onClose={handleCloseModal}>
+        <h2></h2>
+        <p></p>
+      </Modal>
+    </div>
+  );
 }
 
 
 
-document.querySelectorAll('.cardImage').forEach(image => {
-    image.addEventListener('click', () => {
-      image.classList.toggle('clicked');
-    });
-  });
 
 export default Card
